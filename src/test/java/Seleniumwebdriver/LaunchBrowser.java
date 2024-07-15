8959903b7e327edc734f80003ef9abe5d4171ed7
DUDU <DUDU@DUDU>
2024-07-15 18:42:09 +0530

    maximize commands added

--- src/test/java/Seleniumwebdriver/LaunchBrowser.java
+++ src/test/java/Seleniumwebdriver/LaunchBrowser.java
@@ -8,6 +8,7 @@ public class LaunchBrowser {
 		
 		
 		ChromeDriver driver = new ChromeDriver();
-		//driver.get("https://www.amazon.in/");
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
 		}
 }
