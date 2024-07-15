95cc537d4da498b04f79aef65864602cf52db469
DUDU <DUDU@DUDU>
2024-07-15 18:29:31 +0530

    amazon2 updated

---
+++ src/test/java/Seleniumwebdriver/amazon2.java
@@ -0,0 +1,13 @@
+package Seleniumwebdriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class amazon2 {
+
+	public static void main(String[] args) {
+		
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		}
+}
